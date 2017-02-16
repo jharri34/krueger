6764707219003d7293adc8dd4655356126f17370
Jamall <jamall@cisadmins-MacBook-Air.local>
2017-02-16 12:29:23 -0600

    updated gitignore and README

---
+++ pathMapper.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+DATADIR=$1
+if [ -z $DATADIR ]; then
+  echo "Usage: pathMapper [DATA-DIRECTORY] [-u username][-p password][-d database] [-t table] [-h help] [-c column]"
+  exit
+fi
+
+for i in "$@"
+do
+  case $i in
+    -u=*|--username=*)
+    USERNAME="${i#*=}"
+    shift
+    ;;
+    -p=*|--password=*)
+    PASSWORD="${i#*=}"
+    shift
+    ;;
+    -h|--help)
+    echo "Usage: sh pathMapper <DATA-DIRECTORY> <METADATA> [-p password] [-u username] [-d database] [-t table] [-h help] [-c column]"
+    shift
+    ;;
+    -d=*|--database=*)
+    DATABASE="${i#*=}"
+    shift
+    ;;
+    -t=*|--table=*)
+    TABLE_NAME="${i#*=}"
+    shift
+    ;;
+    -c=*|--column=*)
+    COLUMN_NAME="${i#*=}"
+    shift
+    ;;
+    --default)
+    USERNAME="root"
+    PASSWORD="password"
+    shift
+    ;;
+    *)
+  esac
+done
+
+for file in "$DATADIR"/*
+do
+  FULLPATH=$file
+  FILE=${file##*/}
+  FILENAME=${FILE%%.*}
+  EXT=${file##*.}
+  echo "fullpath:$FULLPATH"
+  echo "file:$FILE"
+  echo "filename:$FILENAME"
+  echo "extension:$EXT"
+
+done
+$(mysql -u$USERNAME -p$PASSWORD
+# "SELECT NOT EXISTS (SELECT * FROM $TABLE_NAME WHERE sample=$FILENAME)
+# \BEGIN ALTER $TABLE_NAME ADD "
+# "IF EXISTS (SELECT * FROM INFOMATION_SCHEMA.COLUMNS \
+# WHERE TABLE_NAME='$TABLE_NAME' AND COLUMN_NAME='$COLUMN_NAME')\
+# BEGIN\
+# ALTER TABLE $TABLE_NAME ADD $COLUMN_NAME VARCHAR(255)" | mysql -u$USERNAME -p$PASSWORD $DATABASE;
