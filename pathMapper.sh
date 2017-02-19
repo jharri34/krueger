91e43fcc841b7fc94fcb4b4c3252499d422da727
Jamall <jamall@cisadmins-MacBook-Air.local>
2017-02-19 15:40:12 -0600

    iterating is fun wish i was done

--- pathMapper.sh
+++ pathMapper.sh
@@ -44,7 +44,7 @@ done
 
 # (mysql -D$DATABASE -u$USERNAME -p$PASSWORD -se "SELECT sample FROM $TABLE_NAME WHERE $COLUMN_NAME IS NULL;")
 while sample; do
-  find $DATADIR -name '$sample.*'
+  find $DATADIR -name $sample.*
 done | mysql -D$DATABASE -u$USERNAME -p$PASSWORD -se "SELECT sample FROM $TABLE_NAME WHERE $COLUMN_NAME IS NULL;"
 
 for file in "$DATADIR"/*
