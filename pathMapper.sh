a079693236da74f4e6d63afb9b90687e08f81ec9
Jamall <jamall@cisadmins-Air.internal.redmountainmakers.org>
2017-02-21 20:34:34 -0600

    stupid find

--- pathMapper.sh
+++ pathMapper.sh
@@ -47,7 +47,7 @@ IFS="
 "
 SAMPLES=($(mysql -D$DATABASE -u$USERNAME -p$PASSWORD -se "SELECT sample FROM $TABLE_NAME WHERE $COLUMN_NAME IS NULL;"))
 IFS="$OIFS"
-for sample in "${SAMPLES[@]}"; do
+for SAMPLE in "${SAMPLES[@]}"; do
   echo find "$DATADIR" -name "${SAMPLE}.*"
 done
 #do
