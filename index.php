02e47cbc3dfa054cf27acf52aa5df7b29d71493b
Jamall Harris <harrisjayjamall@gmail.com>
2017-03-29 08:50:52 -0500

    really need to working on some type of remote editing tool

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
  </head>
 	 <body>
 		<form action="upload.php" method="post" enctype="multipart/form-data">
-			Sample:<input type="text" name="sample" />
-			Attachment:<input type="text" name="attachment" />
-			State:<input type="text" name="state" />
-			Ploidy:<input type="text" name="ploidy" />
+			Sample: <input type="text" name="sample" />
+			Attachment: <input type="text" name="attachment" />
+			State: <input type="text" name="state" />
+			Ploidy: <input type="text" name="ploidy" />
 			 File: <input name="userfile[]" type="file" multiple=""/>
     			<input type="submit" value="Send File" />
 		</form>
--- upload.php
+++ upload.php
@@ -3,14 +3,15 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
-$total = count($_FILES[sample][name]);
-for ($i=0; $i<$total; $i++){
-  $tempfile = $_FILES['userfile']['tmp_name'][$i]
-  $fullpath = '/home/kruegerdata/'.$_FILES['userfile']['name'][$i]
-  print_r("tempfile=".$tempfile);
-  print_r("fullpath=".$fullpath);
-  move_uploaded_file($tempfile,$fullpath);
-}
+echo '<pre>'.print_r($_FILES).'</pre>';
+// $total = count($_FILES[sample][name]);
+// for ($i=0; $i<$total; $i++){
+//   $tempfile = $_FILES['userfile']['tmp_name'][$i]
+//   $fullpath = '/home/kruegerdata/'.$_FILES['userfile']['name'][$i]
+//   print_r("tempfile=".$tempfile);
+//   print_r("fullpath=".$fullpath);
+//   move_uploaded_file($tempfile,$fullpath);
+// }
 $sample = $_POST['sample'];
 $attachment = $_POST['attachment'];
 $state = $_POST['state'];
